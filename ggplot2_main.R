b8273892c8702261ffcb28969fa412f4796bc22c
Colin Gillespie <csgillespie@gmail.com>
2012-11-20 21:36:54 +0000

    Tidied up code

--- ggplot2_main.R
+++ ggplot2_main.R
@@ -1,54 +1,67 @@
-library(ggplot2)
-library(plyr)
-library(reshape2)
+require(ggplot2)
+require(plyr)
+require(reshape2)
 
 #############################
 ##Figure 1 of main document
-############################
 ##PCA 
+############################
+
 dd_pca = exp_fac
 dd_pca$tps = factor(dd_pca$tps)
 dd_pca$x = yeast.scores[,1]
 dd_pca$y = yeast.scores[,2]
-ggplot(dd_pca, aes(x, y)) + 
-    geom_point(aes(colour=strain, pch=tps)) + 
-    xlab("PC 1") + ylab("PC 2")
 
-############################
+f1 = ggplot(dd_pca, aes(x, y)) + 
+    geom_point(aes(colour=strain, pch=tps)) + 
+    xlab("PC 1") + ylab("PC 2") + 
+    scale_shape_discrete(name = "Time\n points") +
+    scale_colour_discrete(name = "Strain")
+
+#########################################
 ##Figure 2 of main document
-############################
-##PCA 
+##Time course expression levels for the
+##top 9 differently expressed genes
+#########################################
 top_9_genes = modFordered[1:9]
 gene_id = c_df$probe[top_9_genes]
 name = c_df$genename[top_9_genes]
-exprs.row = yeast.matrix[top_9_genes,]
+exprs_row = yeast.matrix[top_9_genes,]
 
-dd = melt(exprs.row) 
-dd$Var2 = as.character(dd$Var2)
-dd = ddply(dd, .(Var1, Var2), summarise, 
+dd_top_9 = melt(exprs_row) 
+dd_top_9$Var2 = as.character(dd_top_9$Var2)
+##Calculate the means values over replicates
+##Also calculate ymin and ymax in case we want error bars
+dd_top_9 = ddply(dd_top_9, .(Var1, Var2), summarise, 
         m = mean(value), ymax = max(value), ymin=min(value),
         name = name[gene_id==Var1])
-dd$Strain = substr(dd$Var2, 1, 1)
-dd$tps = substr(dd$Var2, 2, nchar(dd$Var2))
-dd$tps = as.numeric(dd$tps)
-dd$name =factor(dd$name, levels=name)
 
-ggplot(dd, aes(tps)) + 
+##Take the first character for strain
+dd_top_9$Strain = substr(dd_top_9$Var2, 1, 1)
+dd_top_9$tps = substr(dd_top_9$Var2, 2, nchar(dd_top_9$Var2))
+dd_top_9$tps = as.numeric(dd_top_9$tps)
+dd_top_9$name = factor(dd_top_9$name, levels=name)
+
+f2 = ggplot(dd_top_9, aes(tps)) + 
     geom_line(aes(y=m, colour=Strain)) +
     geom_point(aes(y=m, colour=Strain)) +
     facet_wrap(~name, ncol=3) + 
-    xlab("Time") + ylab("Expression level")
+    xlab("Time") + 
+    ylab("Expression level")
 
+######################################################
 ##Figure 3 of volcano plot
+##Highlight genes that have an absolute 
+##fold change > 2 and a p-value < Bonferroni cut-off
+######################################################
 
-##Highlight genes that have an absolute fold change > 2 and a p-value < Bonferroni cut-off
-gene_list = topTable(eb, coef=1, number=1000000, sort.by="logFC")
-no_of_genes = nrow(gene_list)
-gene_list$threshold = as.factor(abs(gene_list$logFC) > log2(2) 
+dd_vol = topTable(eb, coef=1, number=1000000, sort.by="logFC")
+no_of_genes = nrow(dd_vol)
+dd_vol$threshold = as.factor(abs(gene_list$logFC) > log2(2) 
                                 & gene_list$P.Value < 0.05/no_of_genes)
 
-##Construct the plot object
-ggplot(data=gene_list, aes(x=logFC, y=-log10(P.Value), colour=threshold)) +
+## Construct the plot object
+f3 = ggplot(dd_vol, aes(logFC, -log10(P.Value), colour=threshold)) +
     geom_point(alpha=0.4, size=1.75) +
     theme(legend.position = "none") +
     geom_hline(yintercept=-log10(0.05/5900), alpha=0.3) + 
@@ -57,39 +70,6 @@ ggplot(data=gene_list, aes(x=logFC, y=-log10(P.Value), colour=threshold)) +
     xlab(expression(paste(log[2], " fold change"))) + 
     ylab(expression(paste(-log[10], " p-value"))) +
     ylim(c(0, 30))
-    
-#Figure 4
-#Heat map
-head(m)
-melt_m = melt(m)
-head(melt_m)
-ggplot(melt_m, aes(Var1, Var2)) + 
-    geom_raster(aes(fill=value))
-
-?hclust
-m1 = m[1:50,]
-l= hclust(dist(m1), method='average')
-melt_m = melt(m1)
-
-l$order
-head(melt_m)
-melt_m$Var1 = factor(melt_m$Var1, levels=hv$rowInd)
-hv$rowInd
-l$order
-
-ggplot(melt_m, aes(Var2, Var1)) + 
-    geom_raster(aes(fill=value)) + 
-    scale_fill_gradient2(low="red", high="green") #+ 
-    scale_x_continuous(expand=c(0, 0)) #+ 
-    scale_y_continuous(expand=c(0, 0)) 
-
-
-
-
-
-
-
-
 
 
 
--- ggplot2_supplemental_figures.R
+++ ggplot2_supplemental_figures.R
@@ -1,126 +1,135 @@
 require(ggplot2)
 
-##Code for creating ggplot2 versions of the paper figures.
+##Code for creating ggplot2 versions of
+##the figures in the supplemental material
 
-###############################
-#Figure 1 of the supp material#
-###############################
-
+#########################################
+## Figure 1 of the supp material        #
+## Image plots of the probe intensities #
+#########################################
 ##Extract the mutant strains in order
-##Change =="m" to "w" for wild type
+##Change == "m" to "w" for wild type
 mut_expr_raw = exprs(yeast.raw[,subset(exp_fac,strain=="m")$data_order])
 
 ##Create a ggplot2 friendly data frame
 ##The data is in the correct order
-dd_g = data.frame(m=as.vector(mut_expr_raw), 
+dd_int = data.frame(m=as.vector(mut_expr_raw), 
                   x = 1:496, 
                   y=rep(1:496, each=496))
-dd_g$tps = rep(c(0, 60, 120, 180, 240), each=496^2)
-dd_g$replicate = rep(1:3, each=496^2*5)
+dd_int$tps = rep(c(0, 60, 120, 180, 240), each=496^2)
+dd_int$replicate = rep(1:3, each=496^2*5)
 
-head(dd_g)
-##ggplot2 function
-##opts removes axis labels
+##theme removes axis labels
 ##scale_ removes tick marks and border
-ggplot(dd_g) + geom_raster(aes(x,y,fill=log2(m)), show_guide=FALSE) + 
+s1 = ggplot(dd_int) + 
+    geom_raster(aes(x,y,fill=log2(m)), show_guide=FALSE) + 
     facet_grid(replicate ~ tps) + 
     theme(axis.text.x=element_blank(), axis.title.x=element_blank(), 
          axis.text.y=element_blank(), axis.title.y=element_blank()) +
     scale_x_continuous(expand=c(0, 0), breaks=NULL) + 
     scale_y_continuous(expand=c(0, 0), breaks=NULL)
 
-#####################################
-##Figure 2 in supplemental material##
-#####################################
+##########################################
+##Figure 2 in supplemental material     ##
+##Density plots of the log2 intensities ##
+##########################################
 #By default, the intensities are only plotted for the pm probes.
-Index = unlist(indexProbes(yeast.raw[,1:30], which = "pm"))
-intensities = as.vector(intensity(yeast.raw[,1:30])[Index, , drop = FALSE])
+index = unlist(indexProbes(yeast.raw[,1:30], which = "pm"))
+intensities = as.vector(intensity(yeast.raw[,1:30])[index, , drop = FALSE])
 no_of_probes = length(intensities)/30
 
 #Construct a data frame for ggplot2
-dd_g = data.frame(intensities=log2(intensities), 
+dd_den = data.frame(intensities=log2(intensities), 
         tps = rep(exp_fac$tps, each=no_of_probes),
         replicate = factor(rep(exp_fac$replicate, each=no_of_probes)),
         strain = rep(exp_fac$strain, each=no_of_probes))
-dd_g$strain = factor(dd_g$strain, labels=c("Mutant", "Wild type"))
+dd_den$strain = factor(dd_den$strain, labels=c("Mutant", "Wild type"))
 
-ggplot(dd_g) + 
+s2 = ggplot(dd_den) + 
     geom_density(aes(x=intensities, colour=replicate)) + 
     facet_grid(tps ~ strain) + 
     ylab("Density") + 
-    xlab("Log (base 2) intensities") 
+    xlab(expression(paste(log[2], " intensities"))) + 
+    theme_bw()
+s2
 
-#####################################
-##Figure 3 in supplemental material##
-#####################################
+#################################################
+##Figure 3 in supplemental material             #
+##Boxplot of the raw and normalised intensities #
+#################################################
 ##getMethod("boxplot","AffyBatch")
 ##getMethod("boxplot","ExpressionSet")
+
 ##The boxplots also have range set to 0.
 ##This corresponds to Inf in ggplot2. 
 ##Default is 1.5
-Index = unlist(indexProbes(yeast.raw, which = "pm"))
-intensities = as.vector(intensity(yeast.raw)[Index, , drop = FALSE])
+index = unlist(indexProbes(yeast.raw, which = "pm"))
+intensities = as.vector(intensity(yeast.raw)[index, , drop = FALSE])
 no_of_probes = length(intensities)/30
 
 #Construct a data frame for ggplot2
-dd_raw = data.frame(intensities=log2(intensities),
+dd_box_raw = data.frame(intensities=log2(intensities),
     tps = rep(exp_fac$tps, each=no_of_probes),
     replicate = factor(rep(exp_fac$replicate, each=no_of_probes)),
     strain = rep(exp_fac$strain, each=no_of_probes))
-dd_raw$type = "Raw"
+dd_box_raw$type = "Raw"
 
 no_of_probes = length(as.vector(yeast.matrix))/30
-dd_rma = data.frame(intensities=as.vector(yeast.matrix),
+dd_box_rma = data.frame(intensities=as.vector(yeast.matrix),
                     tps = rep(exp_fac$tps, each=no_of_probes),
                     replicate = factor(rep(exp_fac$replicate, 
                                            each=no_of_probes)),
                     strain = rep(exp_fac$strain, each=no_of_probes))
-dd_rma$type = "RMA"
+dd_box_rma$type = "RMA"
 
-
-##Boxplots of dd_raw
-ggplot(dd_raw) + 
-    stat_boxplot(aes(y=intensities, x=replicate), coef=Inf) +
+##Boxplots of Raw intensities
+s3a = ggplot(dd_box_raw) + 
+    stat_boxplot(aes(replicate, intensities), coef=Inf) +
     facet_grid(tps ~ strain) +  
     ylab("Density") + 
-    xlab("Log (base 2) intensities") + 
-    ylim(c(2, 16))
+    xlab(expression(paste(log[2], " intensities"))) + 
+    coord_cartesian(ylim=c(2, 16))
 
-##Boxplots of dd_rma
-ggplot(dd_rma) + 
-    stat_boxplot(aes(y=intensities, x=replicate), coef=100) + 
+##Boxplots of transformed intensities
+s3b = ggplot(dd_box_rma) + 
+    stat_boxplot(aes(replicate, intensities), coef=Inf) + 
     facet_grid(tps ~ strain) +  
     ylab("Density") + 
-    xlab("Log (base 2) intensities")  + 
-    ylim(c(2, 16))
+    xlab(expression(paste(log[2], " intensities"))) + 
+    coord_cartesian(ylim=c(2, 16))
 
 ##Boxplots of box raw and rma
-ggplot(rbind(dd_raw, dd_rma)) + 
-    stat_boxplot(aes(y=intensities, x=replicate,  colour=type), coef=100) + 
+s3_joint = ggplot(rbind(dd_box_raw, dd_box_rma)) + 
+    stat_boxplot(aes(replicate, intensities, colour=type), coef=100) + 
     facet_grid(tps ~ strain) +  
     ylab("Density") + 
-    xlab("Log (base 2) intensities")
-
+    xlab(expression(paste(log[2], " intensities"))) + 
+    scale_colour_discrete(name = "Type")
+    
 
 ############################################
-#Figure 4: timecourse package ##############
+##Figure 4: timecourse package            ##
+##Top differentially expressed gene       ##
 ############################################
-gene_positions = MB.2D$pos.HotellingT2[1:1]
+n = 9
+gene_positions = MB.2D$pos.HotellingT2[1:n]
 gnames = rownames(yeast.matrix)
 gene_probes = gnames[gene_positions]
+cn = colnames(yeast.matrix)
 
-dd = data.frame(values = as.vector(yeast.matrix[gene_positions,]), 
-                strain=exp_fac$strain, 
-                tps = exp_fac$tps, 
-                replicate = exp_fac$replicate)
+head(yeast.matrix)
+dd_tc = data.frame(values = as.vector(yeast.matrix[gene_positions,]))
+dd_tc$Strain = rep(substr(cn, 1, 1), each=n)
+dd_tc$tps = rep(substr(cn, 2, nchar(cn)), each=n)
+dd_tc$tps = as.numeric(dd_tc$tps)
+dd_tc$Replicate = factor(rep(c(1:3, 1:3), each=5*n))
+dd_tc$names = gnames[gene_positions]
+dd_tc$names = factor(dd_tc$names, labels = gnames[gene_positions])
 
-ggplot(dd, aes(x=tps, y=values)) + 
-    geom_point(aes(colour=strain, shape=factor(replicate)))
-
-plot(yeast.matrix[gene_positions,1:5])
-
-
-
-
+ggplot(dd_tc, aes(tps, values)) + 
+    geom_point(aes(colour=Strain, shape=Replicate)) + 
+    facet_wrap(~names, ncol=3) + 
+    xlab("Time") + 
+    ylab("Expression level")
 
 
